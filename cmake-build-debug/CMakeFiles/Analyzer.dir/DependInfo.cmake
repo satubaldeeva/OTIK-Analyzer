9cbac2f46dd608ef3d3ffcc2210837513c6a8808
Zhania Satubaldeeva <zhaniasatubaldeeva@yandex.ru>
2022-11-01 19:54:47 +0300

    Add some function for work with pair of symbols

---
+++ cmake-build-debug/CMakeFiles/Analyzer.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/NITRO 5/CLionProjects/OTIK-Analyzer/Analyzer.cpp" "CMakeFiles/Analyzer.dir/Analyzer.cpp.obj" "gcc" "CMakeFiles/Analyzer.dir/Analyzer.cpp.obj.d"
+  "C:/Users/NITRO 5/CLionProjects/OTIK-Analyzer/functions.cpp" "CMakeFiles/Analyzer.dir/functions.cpp.obj" "gcc" "CMakeFiles/Analyzer.dir/functions.cpp.obj.d"
+  "C:/Users/NITRO 5/CLionProjects/OTIK-Analyzer/main.cpp" "CMakeFiles/Analyzer.dir/main.cpp.obj" "gcc" "CMakeFiles/Analyzer.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Analyzer.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "Analyzer.exe"
+  "Analyzer.exe.manifest"
+  "Analyzer.pdb"
+  "CMakeFiles/Analyzer.dir/Analyzer.cpp.obj"
+  "CMakeFiles/Analyzer.dir/Analyzer.cpp.obj.d"
+  "CMakeFiles/Analyzer.dir/functions.cpp.obj"
+  "CMakeFiles/Analyzer.dir/functions.cpp.obj.d"
+  "CMakeFiles/Analyzer.dir/main.cpp.obj"
+  "CMakeFiles/Analyzer.dir/main.cpp.obj.d"
+  "libAnalyzer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Analyzer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
